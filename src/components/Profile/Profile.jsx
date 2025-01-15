a4f2a16f460e87f60d16816a68f077ae99e35857
rodrilv <rott.9954@gmail.com>
2025-01-15 17:54:43 -0600

    hotfix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,14 @@ import { FaGithub } from "react-icons/fa";
 import "./Profile.sass";
 
 const Profile = () => {
+
+  const redirectTo = (site) => {
+    return document.getElementById(site).click();
+  }
   return (
     <>
       <div className="profile-container">
-
+        <a id="linkedin" href="https://www.linkedin.com/in/rodrigo-s%C3%A1nchez-garc%C3%ADa-426b401aa/"></a>
         <div className="profile-picture">
           <img src={ProfileImage}></img>
         </div>
@@ -16,8 +20,8 @@ const Profile = () => {
           <h1>Rodrigo Sánchez García</h1>
           <h3>Software Engineer</h3>
           <div className="profile-socials">
-            <CiLinkedin className="linkedin" />
-            <FaGithub className="github" />
+            <CiLinkedin onClick={() => redirectTo('linkedin')} className="linkedin" />
+            {/*<FaGithub className="github"/> */}
           </div>
         </div>
 
